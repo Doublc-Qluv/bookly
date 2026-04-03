9c5cd57f9d47cda68610d4a2988b7988866883ff
SylverQG <doublc_qluv@163.com>
2026-04-03 20:27:08 +0000

    4-使用SQLModel操作数据库:连接、生命周期时间与模型

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from src.books.routes import book_router
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"server start")
+    await init_db()
+    yield
+    print(f"server stop")
 
 version = "v1"
 
 app = FastAPI(
+    title="Bookly",
+    description="A RESTful API for managing books",
     version=version,
+    lifespan=life_span,
+
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books")
---
+++ src/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ENV_FILE = os.path.join(BASE_DIR, ".env")
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE,
+        extra="ignore"
+    )
+   
+
+Config = Settings()
