7f8f40c9929ce3c08ca02a0b45cad724bab43c4a
SylverQG <doublc_qluv@163.com>
2026-04-04 13:49:15 +0000

    7-用户账户创建:邮箱与密码hash

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,9 @@ from contextlib import asynccontextmanager
 
 from src.books.routes import book_router
 from src.db.main import init_db
+from src.auth.routes import auth_router
+
+
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -21,4 +24,5 @@ app = FastAPI(
 
 )
 
-app.include_router(book_router, prefix=f"/api/{version}/books")
+app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
